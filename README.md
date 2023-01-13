36dfd9d353740a8823d6f219f470606f05809e4b
1gnoramus <elenwarface@gmail.com>
2023-01-13 23:33:45 +0600

    sprites added. First scene added

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+https://noranekogames.itch.io/yumebackground/download/eyJleHBpcmVzIjoxNjczNjI4MjE3LCJpZCI6NTQ5Mjc5fQ%3d%3d.nwBqBDpC%2bVUNHy9YsM9LJNFelMM%3d
+https://noranekogames.itch.io/yumebackground/download/eyJleHBpcmVzIjoxNjczNjI4MjE3LCJpZCI6NTQ5Mjc5fQ%3d%3d.nwBqBDpC%2bVUNHy9YsM9LJNFelMM%3d
+https://sutemo.itch.io/female-character
+https://sutemo.itch.io/male-character-sprite-for-visual-novel
+https://sutemo.itch.io/female-mature-anime-sprite
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
@@ -5,4 +6,48 @@ void main() {
   runApp(GameWidget(game: DakoshaGame()));
 }
 
-class DakoshaGame extends FlameGame {}
+class DakoshaGame extends FlameGame {
+  SpriteComponent dakoshaComponent = SpriteComponent();
+  SpriteComponent tylerComponent = SpriteComponent();
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    //add bg image
+    add(
+      SpriteComponent(
+        size: size,
+        sprite: await loadSprite('bgs/Street.png'),
+      ),
+    );
+    //add char images
+    Vector2 characterSize = Vector2(size.y * .55, size.x * .33);
+    tylerComponent
+      ..size = characterSize
+      ..sprite = await loadSprite('characters/TylerSmile.webp')
+      ..position = Vector2.all(55);
+    add(tylerComponent);
+    dakoshaComponent
+      ..size = characterSize
+      ..sprite = await loadSprite('characters/DakoshaSmile.webp')
+      ..position = Vector2(size.x * .55, 55);
+    add(dakoshaComponent);
+  }
+
+  @override
+  void update(double dt) {
+    if (dakoshaComponent.x > size.x / 2) {
+      dakoshaComponent.x -= 100 * dt;
+    }
+    super.update(dt);
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+
+    final rect = Rect.fromLTWH(0, size.y * .69, size.x, size.y * .33);
+    canvas.drawRect(rect, Paint()..color = Color.fromARGB(140, 191, 190, 190));
+  }
+}
